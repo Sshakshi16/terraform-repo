5eac19d404d1ba3f2ea5aa4bc1d68659c384263c
root <root@ip-172-31-45-43.ap-south-1.compute.internal>
2023-09-03 08:57:25 +0000

    Adding main manifest

---
+++ main.tf
@@ -0,0 +1,18 @@
+provider "aws" {
+        region = "ap-south-1"
+}
+terraform {
+	backend "s3" {
+		bucket = "terraform-storage-bucket"
+		key = "terraform.tfstate"
+		region = "ap-south-1"
+	}
+}
+resource "aws_instance" "new_inst" {
+        ami = "ami-0f5ee92e2d63afc18"
+        instance_type = "t2.micro"
+	tags = {
+		Name = "tf_new_inst"
+	}
+}
+
